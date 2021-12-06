f09d9bcfadbea72a54f99b37685b8ff5cbac85d7
Frankie McEyes <mceyes@protonmail.com>
2021-12-06 14:29:05 +0100

    FIX singola lettera
    Le parole contenenti la schwa finale saranno sostituite. Non verrà effettuata alcuna sostituzione se la schwa è espressa come unica lettera tra due spazi.

--- replace.js
+++ replace.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/ə/gi, 'i');
+            var replacedText = text.replace(/(\w+)ə/gi, '$1i');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
